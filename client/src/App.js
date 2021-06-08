73ea025cf3faa508ce2d04956c8655b736f05efe
srosengren <sebastian@rosengren.me>
2021-06-08 21:37:51 +0200

    Added API

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const value = 'World';
+  const [value, setValue] = useState("World");
+
+  useEffect(() => {
+    (async function () {
+      const text = await (await fetch(`/api/HttpTrigger1`)).text();
+      setValue(text);
+    })();
+  }, []);
+
   return <div>Hello {value}</div>;
 }
+
 
 export default App;
